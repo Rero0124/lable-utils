cfdc085116e3371a16a8b2257aac83e0973b8bce
Rero0124 <kimseung0930@gmail.com>
2024-12-24 14:53:10 +0900

    chore: 테스트 환경 추가

---
+++ test/src/App.tsx
@@ -0,0 +1,9 @@
+const App: React.FC = () => {
+  return (
+    <>
+      aa
+    </>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const webpack = require('webpack');
+
+const basePath = '/test'
+
+module.exports = (argv) => {
+  const prod = argv.mode === "production";
+
+  return {
+    mode: prod ? "production" : "development",
+    devtool: prod ? "hidden-source-map" : "eval",
+    entry: "." + basePath +  "/src/index.tsx",
+    output: {
+      path: path.join(__dirname, basePath, "/build"),
+      filename: "index.js",
+    },
+    devServer: {
+      port: 3000,
+      hot: true,
+    },
+    resolve: {
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: ["babel-loader", "ts-loader"],
+        },
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+      ],
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        React: "react",
+      }),
+      new HtmlWebpackPlugin({
+        template: '.' + basePath + '/public/index.html',
+        minify: process.env.NODE_ENV === 'production' ? {
+          collapseWhitespace: true,
+          removeComments: true,
+        } : false,
+      }),
+      new CleanWebpackPlugin(),
+    ],
+  }
+};
